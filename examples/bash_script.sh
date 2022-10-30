8443f4112f39dfd5da2f1f453395af88fcf666b8
Zac Holland <zacharyholland@gmail.com>
2022-10-30 16:18:33 +0100

    fixed output formatting bug where white space was not being shown

---
+++ examples/bash_script.sh
@@ -0,0 +1,8 @@
+OUTPUT=$(restic backup /home/zac 2>&1)
+CODE=$?
+
+curl -X POST \
+  -d "source=restic_backup_home_zac" \
+  -d "code=$CODE" \
+  --data-urlencode "output=$OUTPUT" \
+  http://0.0.0.0:8000/events/new
